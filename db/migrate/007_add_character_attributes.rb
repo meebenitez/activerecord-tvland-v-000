226d3fb866ab1c38c1cee94944e9a6c1f77d1592
Mary Benitez <meebenitez@gmail.com>
2017-10-11 01:07:25 +0000

    automatically backed up by learn

--- db/migrate/007_add_character_attributes.rb
+++ db/migrate/007_add_character_attributes.rb
@@ -1,7 +1,6 @@
 class AddCharacterAttributes < ActiveRecord::Migration
   def change
     add_column :characters, :actor_id, :integer
-    add_column :characters, :day, :string
-    add_column :characters, :genre, :string
+    add_column :characters, :catchphrase, :string
   end
 end
