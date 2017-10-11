2e099a2364090e5922c21e0ea78d3692ad1d732e
Mary Benitez <meebenitez@gmail.com>
2017-10-11 01:08:49 +0000

    automatically backed up by learn

--- db/migrate/007_add_character_attributes.rb
+++ db/migrate/007_add_character_attributes.rb
@@ -1,6 +1,6 @@
 class AddCharacterAttributes < ActiveRecord::Migration
   def change
     add_column :characters, :actor_id, :integer
-    add_column :characters, :catchphrase, :string
+    add_column :characters, :catch_phrase, :string
   end
 end
