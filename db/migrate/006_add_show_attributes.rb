27e1e05a82a92a8d7fa1fbfeaf1944494cabd257
Mary Benitez <meebenitez@gmail.com>
2017-10-11 01:02:26 +0000

    automatically backed up by learn

--- db/migrate/006_add_show_attributes.rb
+++ db/migrate/006_add_show_attributes.rb
@@ -1,5 +1,7 @@
 class AddShowAttributes < ActiveRecord::Migration
   def change
-    add_column :shows, :artist_id, :integer
+    add_column :shows, :season, :string
+    add_column :shows, :day, :string
+    add_column :shows, :genre, :string
   end
 end
